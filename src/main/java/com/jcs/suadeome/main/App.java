ce25e8b21dca9c5520cefaa86b0a774c2ea6f727
Jefferson <jcsantosbr@gmail.com>
2017-01-29 11:45:35 +0100

    Adapt system to run in Openshift

--- src/main/java/com/jcs/suadeome/main/App.java
+++ src/main/java/com/jcs/suadeome/main/App.java
@@ -16,7 +16,10 @@ import java.util.List;
 import java.util.Map;
 
 import spark.ResponseTransformer;
+import spark.Spark;
 
+import static java.lang.Integer.parseInt;
+import static java.util.Optional.ofNullable;
 import static spark.Spark.get;
 import static spark.Spark.post;
 
@@ -25,13 +28,19 @@ public class App {
   private static ResponseTransformer toJson = model -> new Gson().toJson(model);
 
   public static void main(String[] args) {
+    configureSpark();
 
     DBI dbi = configureDB();
 
     defineRoutes(dbi);
+  }
 
+  private static void configureSpark() {
+    String ipAddress = getEnv("OPENSHIFT_DIY_IP", "localhost");
+    int port = getIntEnv("OPENSHIFT_DIY_PORT",4567);
 
-
+    Spark.ipAddress(ipAddress);
+    Spark.port(port);
   }
 
   private static void defineRoutes(DBI dbi) {
@@ -52,8 +61,6 @@ public class App {
 
         Handle h = dbi.open();
 
-
-
         Id id = generator.generate(EntityType.PROFESSIONAL);
         String name = request.queryParams("name");
         String phone = request.queryParams("phone");
@@ -61,7 +68,7 @@ public class App {
 
         h.createStatement("INSERT INTO professionals (id, name, phone, service, created_by, created_at) " +
             "VALUES (:id, :name, :phone, :service, :user, :now)")
-            .bind("id", id.getValue() )
+            .bind("id", id.getValue())
             .bind("name", name)
             .bind("phone", phone)
             .bind("service", service)
@@ -70,7 +77,7 @@ public class App {
             .execute();
 
         h.close();
-      } catch (Exception e ) {
+      } catch (Exception e) {
         e.printStackTrace();
       }
 
@@ -82,14 +89,23 @@ public class App {
   private static DBI configureDB() {
     PGPoolingDataSource connectionPool = new PGPoolingDataSource();
     connectionPool.setApplicationName("suadeome");
-    connectionPool.setServerName("localhost");
-    connectionPool.setPortNumber(5432);
+    connectionPool.setServerName( getEnv("OPENSHIFT_POSTGRESQL_DB_HOST",  "localhost"));
+    connectionPool.setPortNumber( getIntEnv("OPENSHIFT_POSTGRESQL_DB_PORT", 5432));
     connectionPool.setDatabaseName("suadeome");
-    connectionPool.setUser("suadeome");
-    connectionPool.setPassword("suadeome");
+    connectionPool.setUser( getEnv("OPENSHIFT_POSTGRESQL_DB_APP_USER", "suadeome"));
+    connectionPool.setPassword(getEnv("OPENSHIFT_POSTGRESQL_DB_APP_PASSWORD", "suadeome"));
     connectionPool.setMaxConnections(10);
 
     return new DBI(connectionPool);
   }
+
+  private static int getIntEnv(String property, int defaultValue) {
+    return parseInt(ofNullable(System.getenv(property)).orElse(Integer.toString(defaultValue)));
+  }
+
+  private static String getEnv(String property, String defaultValue) {
+    return ofNullable(System.getenv(property)).orElse(defaultValue);
+  }
+
 }
 
